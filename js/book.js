cc9aa5f8ec1fe581e9c38ef683c99aea2e9043e4
tatellog <tatellog@gmail.com>
2017-05-08 20:48:19 -0500

    2do commit

--- js/book.js
+++ js/book.js
@@ -43,4 +43,18 @@ $(document).ready(function () {
         belowOrigin: false, // Displays dropdown below the button
         alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
+
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'http://placehold.it/250x250'
+        },
+        limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+        onAutocomplete: function (val) {
+            // Callback function when value is autcompleted.
+        },
+        minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+    });
+
 });
